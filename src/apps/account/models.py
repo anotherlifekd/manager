33b7008a5ac102c5c33c44514d7598e88f11b854
Den <anotherlifekd@gmail.com>
2018-12-25 18:41:24 +0200

    asdasd

--- src/apps/account/models.py
+++ src/apps/account/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
     age = models.PositiveSmallIntegerField(null=True, blank=True)  # TODO validate age >= 18
 
     def __str__(self):
-        return 'Name: {}    Last name: {}    Age: {}    Email: {}'.format(self.first_name, self.last_name, self.age, self.email)
+        return 'Name: {}    Last name: {}    Age: {}    Email: {} '.format(self.first_name, self.last_name, self.age, self.email)
 
 
 
