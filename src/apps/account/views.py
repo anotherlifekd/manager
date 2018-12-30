d057f69b29b02c7bc05eadbc2741ffadd6e6b4fc
Den <anotherlifekd@gmail.com>
2018-12-30 11:44:12 +0200

    new error

--- src/apps/account/views.py
+++ src/apps/account/views.py
@@ -1,12 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from apps.account.models import User
+from django.shortcuts import get_object_or_404
 
 
 def profile(request, id):
-    try:
-        user = User.objects.get(id=id)
-        result = 'Username: {}    Age: {}    First-name: {}    Last-name: {}    Email: {}' \
-            .format(user.username, user.age, user.first_name, user.last_name, user.email)
-        return HttpResponse(result)
-    except:
-        return HttpResponse('<h1>404 Page Not Found!</h1>')
+    # два варианта ошибки
+    # try:
+    #     user = User.objects.get(id=id)
+    #     result = 'Username: {}    Age: {}    First-name: {}    Last-name: {}    Email: {}' \
+    #         .format(user.username, user.age, user.first_name, user.last_name, user.email)
+    #     return HttpResponse(result)
+    # except User.DoesNotExist:
+    #     raise Http404("No MyModel matches the given query.")
+    user = get_object_or_404(User, pk=id)
+    result = 'Username: {}    Age: {}    First-name: {}    Last-name: {}    Email: {}' \
+        .format(user.username, user.age, user.first_name, user.last_name, user.email)
+    return HttpResponse(result)
