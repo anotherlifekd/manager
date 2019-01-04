d55544c7b6b37517983bed5db4474c1b63963a24
Den <anotherlifekd@gmail.com>
2019-01-04 13:31:28 +0200

    parse-movies

---
+++ movies/main.py
@@ -0,0 +1,74 @@
+from pdb import set_trace
+import random
+import requests
+from bs4 import BeautifulSoup
+
+ROOT_URL = 'https://ua.kinorium.com/movies/cinema/'
+MOVIES_URL = 'https://ua.kinorium.com'
+MOVIES_LIST_URL = []
+
+with open('./cinema.txt', 'w') as file:
+    response = requests.get(ROOT_URL)
+    # random_sleep()
+    assert response.status_code == 200
+    html_doc = response.text
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    movies_list = soup.findAll('div', {'class': 'filmList__item-title-wrap'})
+    for movie in movies_list:
+        a = movie.find_all('a', href=True)[0]
+        href = a['href']
+        MOVIES_LIST_URL.append(href)
+
+    for film_url in MOVIES_LIST_URL:
+        film = requests.get(MOVIES_URL + film_url)
+        html_doc_film = film.text
+        soup_film = BeautifulSoup(html_doc_film, 'html.parser')
+
+        #get movie name
+        name = soup_film.find('span', {'class': 'film-page__itemprop'})
+        film_name = name.text
+
+        #get movie year
+        full_info = soup_film.find_all('td')[1]
+        film_year = full_info.find('a').text
+
+        #get duration
+        duration = soup_film.find_all('td')[5].text
+
+        #get movie country
+        country = soup_film.find_all('td')[3].find_all('a')
+        country_list = []
+        for i in country:
+            country_list.append(i.text)
+
+        #rank IMDb
+        rank = soup_film.find('ul', {'class': 'ratingsBlock'})
+        imdb = rank.findAll('span')[6].text
+
+        #Genre
+        genre = soup_film.find_all('a', itemprop='genre')
+        genre_list = []
+        for name_genre in genre:
+            genre_list.append(name_genre.text)
+
+        #Budget
+        film_budget = ''
+        rows = soup_film.find_all('tr')
+        for row in rows:
+            cols = row.find_all('td', {'class': 'data'})
+            for col in cols:
+                budget = str(col.text)
+                if budget.startswith(' $'):
+                    film_budget = budget
+
+        #Description
+        description = soup_film.find('div', {'class': 'film-page__text'}).text
+
+        file.write(f'Назва фільму: {film_name}\nрік: {film_year}\nкраїна: {country_list}\n'
+                   f'тривалість: {duration}\nбюджет: {film_budget}\nIMDb: {imdb}\nжанр: {genre_list}\n'
+                   f'опис: {description}\nurl: https://ua.kinorium.com{film_url}\n\n\n')
+        # print(film_url)
+        #print(film_name, film_year, *country_list, imdb, *genre_list, duration, film_budget, description)
+
+
+set_trace()
--- parse_workua/main.py
+++ parse_workua/main.py
@@ -1,16 +1,30 @@
 from pdb import set_trace
-
+from time import sleep
+import random
 import requests
 from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+
 ROOT_URL = 'https://www.work.ua/ru/jobs/'
 
-result_dict = {}
-page = 0
 
-with open('./base-workua.txt', 'w') as file:
-    while page != 4:
+def random_sleep():
+    sleep(random.randint(1, 3))
+
+useragent = UserAgent()
+page = 0
+ids = set()
+
+with open('./workua.txt', 'w') as the_file:
+    while True:
+        # while page != 4:
         page += 1
-        response = requests.get(ROOT_URL, params={'page': page})
+        headers = {
+            'User-Agent': useragent.random,
+        }
+        print('Page: ', page)
+        response = requests.get(ROOT_URL, params={'page': page}, headers=headers)
+        # random_sleep()
         assert response.status_code == 200
         html_doc = response.text
         soup = BeautifulSoup(html_doc, 'html.parser')
@@ -20,11 +34,12 @@ with open('./base-workua.txt', 'w') as file:
             break
 
         cards = job_list.findAll('div', {'class': 'card-hover'})
-
         for card in cards:
             a = card.find_all('a', href=True)[0]
             href = a['href']
+
             id_ = href.split('/')[-2]
             title = a['title']
-            file.write(f'id:{id_};href:{href};title:{title}\n')
-set_trace()
+            if id_ not in ids:
+                the_file.write(f'id:{id_};href:{href};title:{title}\n')
+ids.add(id_)
